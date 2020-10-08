0a7a54f276f91cdab6f8fadfac7a3cad07f342c8
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2020-10-08 14:36:41 +0100

    test local gas estimate

--- contracts/Transfer.sol
+++ contracts/Transfer.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.6.0;
 import "../interfaces/OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/ERC20.sol";
 import "../interfaces/OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/SafeERC20.sol";
 import "../interfaces/OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/access/Ownable.sol";
+import "./GasToken.sol";
 
 contract Transfer is Ownable {
     using SafeERC20 for ERC20;
     address constant ETHER = address(0);
 
-    constructor() public {}
+    receive() external payable {}
 
     function transfer(
         address _assetAddress,
@@ -20,6 +21,21 @@ contract Transfer is Ownable {
         } else {
             ERC20(_assetAddress).safeTransfer(_transferTo, _transferAmount);
         }
+    }
+
+    function transferWithGasToken(
+        address _assetAddress,
+        address payable _transferTo,
+        uint256 _transferAmount,
+        address _gasToken,
+        uint256 _gasTokenAmount
+    ) public onlyOwner {
+        if (_assetAddress == ETHER) {
+            _transferTo.transfer(_transferAmount);
+        } else {
+            ERC20(_assetAddress).safeTransfer(_transferTo, _transferAmount);
+        }
+        require(GasToken(_gasToken).freeFrom(msg.sender, _gasTokenAmount));
     }
 
     function transferAndDestruct(
---
+++ scripts/local_gas_estimate.py
@@ -0,0 +1,28 @@
+from brownie import accounts, Transfer, Token, GasToken
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+OWNER = accounts[0]
+
+def main():
+    token = Token.deploy("Test Token", "TST", 18, 1e20, {'from': accounts[1]})
+
+    transfer = Transfer.deploy({"from": accounts[0]})
+    print(f"Gas used to deploy the contract {transfer.tx.gas_used}")
+
+    t = accounts[0].transfer(transfer.address, 10**18)
+    print(f"Gas used to pay ETH {t.gas_used}")
+
+    t = transfer.transfer(ZERO_ADDRESS, accounts[1], 10**17, {'from': accounts[0]})
+    print(f"Gas used to pay ETH using the contract contract {t.gas_used}")
+
+    t = transfer.transferAndDestruct(ZERO_ADDRESS, accounts[1], 10**17, {'from': accounts[0]})
+    print(f"Gas used to pay ETH by destoying the contract {t.gas_used}")
+
+    # Token gas usage
+    t = token.transfer(transfer.address, 10**18, {'from':accounts[1]})
+    print(f"Gas used to send ERC20 to a contract {t.gas_used}")
+
+    t = transfer.transfer(token.address, accounts[1], 10**17, {'from': accounts[0]})
+    print(f"Gas used to send ERC20 using the contract {t.gas_used}")
+
+    t = transfer.transferAndDestruct(token.address, accounts[1], 10**17, {'from': accounts[0]})
+    print(f"Gas used to send ERC20 by destoying the contract {t.gas_used}")
