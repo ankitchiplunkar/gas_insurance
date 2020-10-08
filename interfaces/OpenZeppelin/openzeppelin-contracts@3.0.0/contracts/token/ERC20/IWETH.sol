7201e56898c73b8521315b5314d6c52347c9fe21
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2020-10-08 12:26:42 +0100

    added initial interfaces

---
+++ interfaces/OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/IWETH.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.7.0;
+import "./IERC20.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IWETH is IERC20 {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+}
